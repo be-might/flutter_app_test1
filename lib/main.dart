21fb2ec8a4ec7ca1639f7c7f451497ae517d5056
Bohdan <lan8024@gmail.com>
2022-10-03 21:43:46 +0300

    lesson 15

--- lib/main.dart
+++ lib/main.dart
@@ -6,83 +6,24 @@ void main() {
   runApp(MyFirstApp());
 }
 
-class MyFirstApp extends StatefulWidget {
-  @override
-  State<MyFirstApp> createState() => _MyFirstAppState();
-}
-
-class _MyFirstAppState extends State<MyFirstApp> {
-  bool _loading = false;
-  double _prgressValue = 0;
-
-  @override
-  void initState() {
-    _loading = false;
-    _prgressValue = 0;
-    super.initState();
-  }
+class MyFirstApp extends StatelessWidget {
+  const MyFirstApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.indigo,
-        appBar: AppBar(
-          title: const Text('My First app'),
-          centerTitle: true,
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            setState(() {
-              _loading = !_loading;
-              _updateProgress();
-            });
-          },
-          child: const Icon(Icons.cloud_download),
-        ),
+        appBar: AppBar(),
         body: Center(
-          child: Container(
-              padding: const EdgeInsets.all(16),
-              child: _loading
-                  ? Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        LinearProgressIndicator(
-                          value: _prgressValue,
-                        ),
-                        Text(
-                          '${(_prgressValue * 100).round()}%',
-                          style: const TextStyle(
-                            color: Colors.white,
-                            fontSize: 20,
-                          ),
-                        ),
-                      ],
-                    )
-                  : const Text(
-                      'Press button to download',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 20,
-                      ),
-                    )),
+          child: Stack(
+            fit: StackFit.expand,
+            children: [
+              Image(image: AssetImage('assets/images/bg.jpg')),
+              Image(image: AssetImage('assets/icons/icon.png'))
+            ],
+          ),
         ),
       ),
     );
-  }
-
-  void _updateProgress() {
-    const oneSec = Duration(seconds: 1);
-    Timer.periodic(oneSec, (timer) {
-      setState(() {
-        _prgressValue += 0.2;
-        if (_prgressValue.toStringAsFixed(1) == '1.0') {
-          _loading = false;
-          timer.cancel();
-          _prgressValue = 0.0;
-          return;
-        }
-      });
-    });
   }
 }
