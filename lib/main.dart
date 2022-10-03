26b8f10645f4bfb656f843690b3df0e4e2d6f83d
Bohdan <lan8024@gmail.com>
2022-10-03 19:16:07 +0300

    lesson 14

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,27 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyFirstApp());
 }
 
-class MyFirstApp extends StatelessWidget {
+class MyFirstApp extends StatefulWidget {
+  @override
+  State<MyFirstApp> createState() => _MyFirstAppState();
+}
+
+class _MyFirstAppState extends State<MyFirstApp> {
+  bool _loading = false;
+  double _prgressValue = 0;
+
+  @override
+  void initState() {
+    _loading = false;
+    _prgressValue = 0;
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,37 +32,57 @@ class MyFirstApp extends StatelessWidget {
           centerTitle: true,
         ),
         floatingActionButton: FloatingActionButton(
-          onPressed: null,
-          child: Icon(Icons.cloud_download),
+          onPressed: () {
+            setState(() {
+              _loading = !_loading;
+              _updateProgress();
+            });
+          },
+          child: const Icon(Icons.cloud_download),
         ),
         body: Center(
           child: Container(
-            padding: const EdgeInsets.all(16),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: const [
-                LinearProgressIndicator(
-                  value: 23,
-                ),
-                Text(
-                  '23 %',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                  ),
-                ),
-                Text(
-                  'Press button to download',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                  ),
-                )
-              ],
-            ),
-          ),
+              padding: const EdgeInsets.all(16),
+              child: _loading
+                  ? Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        LinearProgressIndicator(
+                          value: _prgressValue,
+                        ),
+                        Text(
+                          '${(_prgressValue * 100).round()}%',
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                          ),
+                        ),
+                      ],
+                    )
+                  : const Text(
+                      'Press button to download',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                      ),
+                    )),
         ),
       ),
     );
+  }
+
+  void _updateProgress() {
+    const oneSec = Duration(seconds: 1);
+    Timer.periodic(oneSec, (timer) {
+      setState(() {
+        _prgressValue += 0.2;
+        if (_prgressValue.toStringAsFixed(1) == '1.0') {
+          _loading = false;
+          timer.cancel();
+          _prgressValue = 0.0;
+          return;
+        }
+      });
+    });
   }
 }
