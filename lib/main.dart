52a9455bb995c1f50af22493f5df553fd1e6253b
Bohdan <lan8024@gmail.com>
2022-10-03 22:31:52 +0300

    lesson 16

--- lib/main.dart
+++ lib/main.dart
@@ -12,14 +12,30 @@ class MyFirstApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(fontFamily: 'IndieFlower'),
       home: Scaffold(
-        appBar: AppBar(),
+        appBar: AppBar(
+          title: Text('Adding Assets'),
+          centerTitle: true,
+        ),
         body: Center(
           child: Stack(
             fit: StackFit.expand,
             children: [
               Image(image: AssetImage('assets/images/bg.jpg')),
-              Image(image: AssetImage('assets/icons/icon.png'))
+              Image(image: AssetImage('assets/icons/icon.png')),
+              Positioned(
+                top: 16,
+                left: 115,
+                child: Text(
+                  'My custom font',
+                  style: TextStyle(
+                    fontSize: 30,
+                    color: Colors.white,
+                    // fontFamily: 'IndieFlower',
+                  ),
+                ),
+              )
             ],
           ),
         ),
